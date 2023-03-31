81615267983636327d58d390558d0002297ad2c4
lalabearchu <lalabearchu@gmail.com>
2023-03-31 19:57:13 +0800

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const express = require('express');
 const app = express();
@@ -35,7 +39,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// extra packages
+
+app.get("/", (req, res) => {
+  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>")
+})
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
