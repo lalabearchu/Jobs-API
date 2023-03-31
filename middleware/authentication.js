0b74e840e613e7808a2255f2bb7070392c137f73
lalabearchu <lalabearchu@gmail.com>
2023-03-31 18:48:42 +0800

    first commit

---
+++ middleware/authentication.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+
+const auth = (req, res, next) => {
+    // check header
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new UnauthenticatedError("Authentication Invalid");
+    }
+    // check token
+    const token = authHeader.split(" ")[1]
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        // attach the user to the job routes
+        // another way
+        // const user = User.findById(payload.userId).select("-password");
+        // req.user = user;
+        req.user = { userId: payload.userId, name: payload.name };
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError("Authentication Invalid");
+    }
+}
+
+
+module.exports = auth;
